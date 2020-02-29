be916c0056e09186a9b1e8c7d3df4bf3afde3207
diegomrz <diegomrz@outlook.com>
2020-02-28 23:51:01 -0300

    #005 - Importando imagens

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
-        }
+        },
       },
       {
         test: /\.css$/,
@@ -24,6 +24,12 @@ module.exports = {
           { loader: 'style-loader' },
           { loader: 'css-loader' }
         ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader'
+        },
       }
     ]
   }
